e3e6934d72e27106bfa881ba3a232685f32c0b9d
Tobias Küchler <tobias-kuechler@outlook.com>
2018-11-26 23:36:26 +0100

    Remove the scraping window

--- src/background.js
+++ src/background.js
@@ -14,7 +14,6 @@ log.info('App starting...');
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 let win;
-let searchWindow;
 
 // Standard scheme must be registered before the app is ready
 protocol.registerStandardSchemes(['app'], { secure: true });
@@ -44,46 +43,6 @@ function createWindow() {
         win = null;
     });
 }
-
-function createScrapingWindow(data) {
-    searchWindow.loadURL(`https://xdcc.horriblesubs.info/?search=${data.name} ${data.quality}`);
-
-    searchWindow.webContents.once('did-navigate', () => {
-        searchWindow.webContents.executeJavaScript(
-            `
-            var script = document.createElement('script');
-            script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js";
-            document.getElementsByTagName('head')[0].appendChild(script);
-
-            setTimeout(() => {
-                var rows = [];
-                var $headers = $("th").clone(true);
-                $headers.text(function(index, text) { return text.substring(0, text.length-5).toLowerCase()});
-                var $rows = $("tbody tr").each(function(index) {
-                    $cells = $(this).find("td");
-                    rows[index] = {};
-                    $cells.each(function(cellIndex) {
-                        rows[index][$($headers[cellIndex]).text()] = $(this).text();
-                    });    
-                });
-
-                var table = {};
-                table = rows;
-                require('electron').ipcRenderer.send('documentResult', table);
-            }, 1000);
-        `
-        );
-    });
-}
-
-ipcMain.on('reloadData', (event, data) => {
-    createScrapingWindow(data);
-});
-
-ipcMain.on('documentResult', (event, data) => {
-    data.shift();
-    win.webContents.send('updateTable', data);
-});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
