8c1136d433796ba0a598c7deb41cf9d2141a7b25
Tobias Küchler <tobias-kuechler@outlook.com>
2018-11-26 22:51:26 +0100

    Prettier / ESLint updates (hopefully works now)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    browser: true,
-  },
-  extends: ['plugin:vue/essential', '@vue/airbnb'],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    indent: ['error', 4],
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'no-shadow': ['error', { allow: ['state', 'getters'] }],
-    'no-underscore-dangle': [1, { allow: ['_id'] }],
-    'no-param-reassign': ['error', { ignorePropertyModificationsFor: ['state'] }],
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  globals: {
-    window: true,
-    $: true,
-  },
+    root: true,
+    env: {
+        node: true,
+        es6: true,
+    },
+    extends: ['plugin:vue/essential', '@vue/airbnb', 'prettier'],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+        'no-shadow': ['error', { allow: ['state', 'getters'] }],
+        'no-underscore-dangle': [1, { allow: ['_id'] }],
+        'no-param-reassign': ['error', { ignorePropertyModificationsFor: ['state'] }],
+    },
+
+    parserOptions: {
+        parser: 'babel-eslint',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: ['prettier', 'vue'],
 };
