d52f58a1ec0fb4ff966433ac07f0ae191acb79d0
Tobias Küchler <tobias-kuechler@outlook.com>
2018-11-28 13:10:48 +0100

    Add search button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,14 @@
 <template>
-    <div style="margin-top: 20px">
+    <div style="margin-top: 20px;">
         <div class="columns">
-            <div class="column is-offset-1 is-10">
+            <div class="column is-offset-1 is-8">
                 <b-field>
-                    <b-input :loading="loading" expanded placeholder="Search.." @blur="reload"/>
+                    <b-input
+                        v-model="searchInput"
+                        expanded
+                        placeholder="Search.."
+                        @keyup.enter.native="reload"
+                    />
                     <b-select v-model="selectedQuality" placeholder="Quality">
                         <option value="1080">1080p</option>
                         <option value="720">720p</option>
@@ -11,8 +16,14 @@
                     </b-select>
                 </b-field>
             </div>
+            <div class="column is-2">
+                <button
+                    :class="{ button: true, 'is-primary': true, 'is-fullwidth': true, 'is-loading': loading }"
+                    :disabled="loading"
+                    @click="reload"
+                >Search</button>
+            </div>
         </div>
-        <div class="columns">
             <div class="column is-10 is-offset-1">
                 <b-table
                     :checked-rows.sync="checkedRows"
