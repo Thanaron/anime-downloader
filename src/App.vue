3ebb06733f7ffe30eb81a205448bc39edace9ee1
Tobias Küchler <tobias-kuechler@outlook.com>
2018-12-18 00:55:19 +0100

    Automatically generate a username

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import SetupModal from './components/SetupModal.vue';
+import axios from 'axios';
 
 const log = require('electron-log');
 const unhandled = require('electron-unhandled');
@@ -22,12 +22,31 @@ export default class App extends Vue {
             !this.$store.state.username ||
             this.$store.state.username.length === 0
         ) {
-            this.$modal.open({
-                component: SetupModal,
-                hasModalCard: true,
-                parent: this,
-            });
+            this.generateRandom();
         }
+    }
+    generateRandom() {
+        this.$store.commit('newStatus', 'Generating username..');
+        return axios
+            .get('https://passwordrandom.com/query', {
+                params: {
+                    command: 'password',
+                    count: 1,
+                    format: 'plain',
+                    scheme: 'cvcvvcv',
+                },
+            })
+            .then(response => {
+                this.setUsername(response.data);
+                this.$store.commit('newStatus', 'Ready');
+            });
+    }
+
+    setUsername(username: string) {
+        this.$store.dispatch('set', {
+            key: 'username',
+            value: username,
+        });
     }
 }
 </script>
